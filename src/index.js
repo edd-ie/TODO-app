af70228ccda070e2c0e210827c75dd8f5638b73a
edd-ie <ogendoeddie@gmail.com>
2023-03-22 08:16:08 +0300

    Compete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
+//TODO:
+//Todo: able to type a task into the input field.
+//Todo: able to click some form of a submit button.
+//Todo: see the task string that I provided appear in the DOM after the submit button is clicked
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById('create-task-form')
+  let button = document.getElementById('btn');
+  let txt = document.getElementById('new-task-description')
+  
+  
+
+  form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    
+    
+    let task = txt.value;
+    newTask(task);
+
+  })
+  
+  let newTask = (val) => {    
+    let content = document.getElementById('tasks');
+    let newTodo = document.createElement('p');
+    let newBtn = document.createElement('button');
+
+    newTodo.textContent = val + " "
+    newBtn.textContent = 'X';
+    newBtn.style = 'height: 15px; font-size: 10px; font-weight: bold; background: red; color: black; border-radius: 5px;'
+    newTodo.style = 'width: 100px;'
+
+    content.appendChild(newTodo);
+    newTodo.appendChild(newBtn);
+
+    form.reset();
+    delTask(newBtn);
+  }
+
+  function delTask(btn){
+    btn.addEventListener('click', ()=>{
+      btn.parentNode.remove()
+    })    
+  }
+
 });
